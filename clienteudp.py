59f09e05d873ed85a0c8f5ba26773e0d00a6d8b4
luizsac <lu_que90@hotmail.com>
2021-08-17 23:14:18 -0300

    desenvolvimento de um cliente udp

---
+++ clienteudp.py
@@ -0,0 +1,23 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # protocolo ipv4, protocolo udp
+
+print("Socket criado com sucesso!")
+
+host = "localhost"
+port = 5433
+message = "Hello again, friend of a friend"
+
+try:
+    print(f"Cliente: {message}")
+    s.sendto(message.encode(), (host, 5432))
+
+    data, server = s.recvfrom(4096)
+    data = data.decode()
+    print(f"Cliente: {data}")
+except socket.error as e:
+    print("Erro na conexão!")
+    print(f"Erro: {e}")
+finally:
+    print("Cliente fechando a conexão")
+    s.close()
